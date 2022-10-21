f98b24c3510e7585ab9b41a73f496f34c1cb53e4
sari7happy <sari7happy@gmail.com>
2022-10-21 18:30:50 +0900

    画像読み込み

--- app/Http/Controllers/ImageTitleController.php
+++ app/Http/Controllers/ImageTitleController.php
@@ -12,7 +12,23 @@ class ImageTitleController extends Controller
   }
   public function store(Request $request)
   {
-dd($request->all());
+    $request->validate([
+        'image'=>'required|image|mimes:png,jpg,jpeg|max:2048'
+    ]);
+    $imageName=time().'_'.$request->image->extension();
+// public Folder
+    $request->image->move(public_path('images'),$imageName);
+
+    // store instorage Folder
+    // $request->image->storeAs('images',$imageName);
+// // store inS3
+//     $request->image->storeAs('images',$imageName,'s3');
+
+
+    // store image inDB
+    return back()->with('success','Image uploaded Successfully!')
+    ->with('image',$imageName);
+
   }
   
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,9 +10,16 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     画像投稿<br><br>
+
+                    @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <strong>{{$message}}</strong>
+                    </div>
+                    <img src="{{asset('images/'.Session::get('image'))}}"/>
+                    @endif
                     <form method="POST" action="{{route('image.store')}}" enctype="multipart/form-data">
                         @csrf
-                        <input type="file" name="image">
+                        <input type="file" class="form-control" name ="image"/>
                         <button type="submit" class="btn btn">アップロード</button>
                     </form>
 
